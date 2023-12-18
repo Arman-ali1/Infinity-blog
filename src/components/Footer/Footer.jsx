ba9a2abffb12c03657088721b6f0a9696bb99c09
Arman Ali <armnsrn173200@gmail.com>
2023-12-18 20:55:14 +0530

    Initial commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -28,9 +28,9 @@ function Footer() {
                                 <li className="mb-4">
                                     <Link
                                         className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                                        to="/"
+                                        to=""
                                     >
-                                        Features
+                                        GitHub
                                     </Link>
                                 </li>
                                 <li className="mb-4">
